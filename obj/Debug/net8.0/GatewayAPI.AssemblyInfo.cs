85c9694ca902b80120b753231c98e695993d010d
Lucas Costa Borges <lucascostaborges2007@gmail.com>
2026-02-19 14:56:46 -0300

    Iniciação e Reinciação de Push e Pull.

--- obj/Debug/net8.0/GatewayAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/GatewayAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GatewayAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05584541987df70a1eab108d4ac7a1110adb8ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GatewayAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GatewayAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
